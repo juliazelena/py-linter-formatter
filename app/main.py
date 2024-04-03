180c0cc48c56039d585e638b5b98674b71c0b36b
ulichka <ulichka7615@gmail.com>
2024-04-03 19:26:47 +0300

    fix tests and code style

--- app/main.py
+++ app/main.py
@@ -12,17 +12,10 @@ def format_single_linter_file(file_path: str, errors: list) -> dict:
     return {
         "errors":
             [
-                {
-                    "line": error.get("line_number"),
-                    "column": error.get("column_number"),
-                    "message": error.get("text"),
-                    "name": error.get("code"),
-                    "source": "flake8",
-                }
-                for error in errors
+                format_linter_error(error) for error in errors
             ],
         "path": file_path,
-        "status": "failed",
+        "status": "failed" if ["errors"] != [] else "success"
     }
 
 
@@ -33,50 +26,10 @@ def format_linter_report(linter_report: dict) -> list:
             "path": "./test_source_code_2.py",
             "status": "passed",
         },
-        {
-            "errors":
-            [
-                {
-                    "line": value.get("line_number"),
-                    "column": value.get("column_number"),
-                    "message": value.get("text"),
-                    "name": value.get("code"),
-                    "source": "flake8",
-                }
-                for value in linter_report["./source_code_2.py"]
-            ],
-            "path": "./source_code_2.py",
-            "status": "failed",
-        },
-        {
-            "errors":
-                [
-                    {
-                        "line": value.get("line_number"),
-                        "column": value.get("column_number"),
-                        "message": value.get("text"),
-                        "name": value.get("code"),
-                        "source": "flake8",
-                    }
-                    for value in linter_report["./source_code_1.py"]
-                ],
-            "path": "./source_code_1.py",
-            "status": "failed",
-        },
-        {
-            "errors":
-                [
-                    {
-                        "line": value.get("line_number"),
-                        "column": value.get("column_number"),
-                        "message": value.get("text"),
-                        "name": value.get("code"),
-                        "source": "flake8",
-                    }
-                    for value in linter_report["./test_source_code_1.py"]
-                ],
-            "path": "./test_source_code_1.py",
-            "status": "failed",
-        },
-
+        format_single_linter_file("./source_code_2.py",
+                                  linter_report["./source_code_2.py"]),
+        format_single_linter_file("./source_code_1.py",
+                                  linter_report["./source_code_1.py"]),
+        format_single_linter_file("./test_source_code_1.py",
+                                  linter_report["./test_source_code_1.py"]),
     ]
