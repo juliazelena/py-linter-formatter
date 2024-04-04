60e134e9bbca6eff4fabcf40ebb9b6319c428e5f
ulichka <ulichka7615@gmail.com>
2024-04-04 16:03:20 +0300

    used the loop and removed unnecessary code

--- app/main.py
+++ app/main.py
@@ -15,21 +15,12 @@ def format_single_linter_file(file_path: str, errors: list) -> dict:
                 format_linter_error(error) for error in errors
             ],
         "path": file_path,
-        "status": "failed" if ["errors"] != [] else "success"
+        "status": "failed" if errors else "passed"
     }
 
 
 def format_linter_report(linter_report: dict) -> list:
     return [
-        {
-            "errors": linter_report["./test_source_code_2.py"],
-            "path": "./test_source_code_2.py",
-            "status": "passed",
-        },
-        format_single_linter_file("./source_code_2.py",
-                                  linter_report["./source_code_2.py"]),
-        format_single_linter_file("./source_code_1.py",
-                                  linter_report["./source_code_1.py"]),
-        format_single_linter_file("./test_source_code_1.py",
-                                  linter_report["./test_source_code_1.py"]),
+        format_single_linter_file(key, value)
+        for key, value in linter_report.items()
     ]
